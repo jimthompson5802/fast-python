114cdf2ef7d97908f3e7e6586785e503b1fa3e82
Jim Thompson <jimthompson5802@gmail.com>
2024-01-01 22:21:41 +0000

    baseline code

--- ple/ple_transformer_cython.pyx
+++ ple/ple_transformer_cython.pyx
@@ -7,10 +7,13 @@ cimport numpy as np
 
 @cython.boundscheck(False)
 def _ple_transform_cython(np.ndarray[np.float64_t, ndim=1] column_data, np.ndarray[np.float64_t, ndim=1] column_bin_boundaries, int num_bins):
+    cdef int num_rows = column_data.shape[0]
+    cdef int i, row, col
+
     cdef np.ndarray[np.float64_t, ndim=2] encoded_data = np.ones((column_data.shape[0], num_bins))
     cdef np.ndarray[np.int64_t, ndim=1] bin_indices = np.digitize(column_data, column_bin_boundaries) - 1
     cdef np.ndarray[np.float64_t, ndim=1] bin_min = column_bin_boundaries[bin_indices]
-    bin_min[bin_indices == num_bins] = column_bin_boundaries[-2]
+    bin_min[bin_indices == num_bins] = column_bin_boundaries[num_bins - 1]
     cdef np.ndarray[np.float64_t, ndim=1] bin_numerator = column_data - bin_min
     cdef np.ndarray[np.float64_t, ndim=1] bin_widths = np.diff(column_bin_boundaries)
     cdef np.ndarray[np.int64_t, ndim=1] idxs = bin_indices
@@ -18,10 +21,11 @@ def _ple_transform_cython(np.ndarray[np.float64_t, ndim=1] column_data, np.ndarr
     cdef np.ndarray[np.float64_t, ndim=1] bin_denominator = bin_widths[idxs]
     cdef np.ndarray[np.float64_t, ndim=1] encoded_values = bin_numerator / bin_denominator
     cdef np.ndarray[np.uint8_t, ndim=2] mask = np.zeros((encoded_data.shape[0], encoded_data.shape[1]), dtype=np.bool_)
-    cdef int i, row, col
+
     for i in range(encoded_data.shape[0]):
         mask[i, bin_indices[i]] = True
     cdef np.ndarray[np.int64_t, ndim=1] rows, cols
+
     rows, cols = np.where(mask)
     for row, col in zip(rows, cols):
         encoded_data[row, col] = encoded_values[row]
