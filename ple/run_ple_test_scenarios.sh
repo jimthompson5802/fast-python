b3bdebdb59616816285d9494fd9fe78438537573
Jim Thompson <jimthompson5802@gmail.com>
2024-01-07 22:13:33 +0000

    Refactor test scenarios script to include multiple trials and update parameters

--- ple/run_ple_test_scenarios.sh
+++ ple/run_ple_test_scenarios.sh
@@ -4,19 +4,23 @@ rm test_results.csv
 
 # array for values 1000, 10000
 # declare -a num_samples=("1000")
-declare -a num_samples=("100000" "200000" "400000" "800000" )
+declare -a num_samples=("100000" "200000" "400000" )
 
 # array of transformer names
 declare -a transformers=("PiecewiseLinearEncoderNumpy" "PiecewiseLinearEncoderNumbaV0" "PiecewiseLinearEncoderNumbaV1" "PiecewiseLinearEncoderCython")
 
-# loop through the array of values
-for i in "${num_samples[@]}"
+# loop trials
+for i in {1..5}
 do
-    # loop through the array of transformer names
-    for j in "${transformers[@]}"
+    echo running trial $i
+    # loop through the array of values
+    for j in "${num_samples[@]}"
     do
-        # run the test scenarios
-        python ple_test_scenarios.py $i 10 $j
+        # loop through the array of transformer names
+        for k in "${transformers[@]}"
+        do
+            # run the test scenarios
+            python ple_test_scenarios.py $j 20 $k
+        done
     done
 done
-
