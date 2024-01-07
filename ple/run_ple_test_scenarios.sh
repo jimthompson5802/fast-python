43cc1257c1e0ecb49cf7866015fde41f4f7a7a1a
Jim Thompson <jimthompson5802@gmail.com>
2024-01-07 13:02:47 +0000

    Add PLE test scenarios and script to run them

---
+++ ple/run_ple_test_scenarios.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm test_results.csv
+
+# array for values 1000, 10000
+declare -a num_samples=("1000" "10000" "100000" "1000000")
+
+# array of transformer names
+declare -a transformers=("PiecewiseLinearEncoderNP" "PiecewiseLinearEncoderNumbaV0" "PiecewiseLinearEncoderNumbaV1" "PiecewiseLinearEncoderCython")
+
+# loop through the array of values
+for i in "${num_samples[@]}"
+do
+    # loop through the array of transformer names
+    for j in "${transformers[@]}"
+    do
+        # run the test scenarios
+        python ple_test_scenarios.py $i 10 $j
+    done
+done
+
